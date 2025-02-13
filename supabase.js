61bfebde622b33671bdbae215d04a7aeb66ec6dd
Yunoxa <saudhmuhammedh@gmail.com>
2025-02-13 19:04:19 +0530

    vf

--- supabase.js
+++ supabase.js
@@ -1,9 +1,11 @@
-import { createClient } from "@supabase/supabase-js";
+import dotenv from 'dotenv'; // Load environment variables
+import { createClient } from '@supabase/supabase-js';
 
-// Replace with your Supabase project URL and API Key
-const SUPABASE_URL = "https://qopdjgfciakmvhlriixt.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvcGRqZ2ZjaWFrbXZobHJpaXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0NDA2NDAsImV4cCI6MjA1NTAxNjY0MH0.5UAelwww7WpDUExqhc5dH2JhUWlGNgUNjh8fzxPNZvs";
+dotenv.config(); // Load variables from .env
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE // Using Service Role Key securely
+);
 
 export default supabase;
